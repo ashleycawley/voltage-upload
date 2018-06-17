3663c416edc55bf775218f5fe8cff9911822bc1b
Ashley Cawley <ash@ashleycawley.co.uk>
2018-06-17 19:29:06 +0000

    Made SCP run in quiet mode and then directed all output to nowhere

--- voltage-upload.sh
+++ voltage-upload.sh
@@ -7,7 +7,7 @@ raspistill -w 640 -h 480 -q 75 -o /home/pi/voltage-upload/voltage.jpg
 sleep 2
 
 # Uploads the photo to the server
-scp -P 2223 -i /home/pi/.ssh/id_rsa_solar voltage.jpg acawley@solar.ashleycawley.co.uk:/home/acawley/solar.ashleycawley.co.uk
+scp -q -P 2223 -i /home/pi/.ssh/id_rsa_solar voltage.jpg acawley@solar.ashleycawley.co.uk:/home/acawley/solar.ashleycawley.co.uk &>/dev/null
 
 # Exits
 exit 0
