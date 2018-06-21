1dc6f4c640b7ed9f9ce7d07e27dd9ef64d3d89e5
Ashley Cawley <ash@ashleycawley.co.uk>
2018-06-21 19:33:52 +0000

    Added a timestamp to the image output

--- voltage-upload.sh
+++ voltage-upload.sh
@@ -6,6 +6,15 @@ raspistill -w 640 -h 480 -q 75 -o /home/pi/voltage-upload/voltage.jpg
 # Pauses for 2 seconds
 sleep 2
 
+for file in voltage.jpg ; do
+   convert /home/pi/voltage-upload/"$file" -font Helvetica-Bold \
+-pointsize 34 -fill white -annotate +25+455  \
+      %[exif:DateTimeOriginal] /home/pi/voltage-upload/"${file}"
+done
+
+# Pauses for 2 seconds
+sleep 2
+
 # Uploads the photo to the server
 scp -q -P 2223 -i /home/pi/.ssh/id_rsa_solar /home/pi/voltage-upload/voltage.jpg acawley@solar.ashleycawley.co.uk:/home/acawley/solar.ashleycawley.co.uk &>/dev/null
 
